7d15a2edf9ea5af21a38d78e6465b4bfd7b456e1
Shubham Sharma <ss250892@gmail.com>
2022-07-06 01:45:24 +0530

    initial commit

--- MovieTicketBookingSystem/src/main/java/com/shubhamsharma/movieticketbooking/MovieTicketBookingSystemApplication.java
+++ MovieTicketBookingSystem/src/main/java/com/shubhamsharma/movieticketbooking/MovieTicketBookingSystemApplication.java
@@ -3,7 +3,7 @@ package com.shubhamsharma.movieticketbooking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class MovieTicketBookingSystemApplication {
 
 	public static void main(String[] args) {
